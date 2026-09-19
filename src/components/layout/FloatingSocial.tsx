717d486ccf050357fa11d381623a832dae0acac8
kennethkclab <291267340+kennethkclab@users.noreply.github.com>
2026-09-19 22:31:29 +0800

    enlarge Douyin and Xiaohongshu marks inside circles

--- src/components/layout/FloatingSocial.tsx
+++ src/components/layout/FloatingSocial.tsx
@@ -46,20 +46,25 @@ export default function FloatingSocial() {
           >
             {item.id === "youtube" ? (
               <span className="flex h-full w-full items-center justify-center bg-[#FF0000] text-white">
-                <svg viewBox="0 0 24 24" className="h-4 w-4" fill="currentColor" aria-hidden>
+                <svg viewBox="0 0 24 24" className="h-[18px] w-[18px]" fill="currentColor" aria-hidden>
                   <path d="M23.5 6.2a3 3 0 0 0-2.1-2.1C19.5 3.7 12 3.7 12 3.7s-7.5 0-9.4.4A3 3 0 0 0 .5 6.2 31.5 31.5 0 0 0 0 12a31.5 31.5 0 0 0 .5 5.8 3 3 0 0 0 2.1 2.1c1.9.4 9.4.4 9.4.4s7.5 0 9.4-.4a3 3 0 0 0 2.1-2.1A31.5 31.5 0 0 0 24 12a31.5 31.5 0 0 0-.5-5.8zM9.8 15.5V8.5L16.2 12 9.8 15.5z" />
                 </svg>
               </span>
             ) : item.id === "douyin" ? (
               <span className="flex h-full w-full items-center justify-center bg-black">
-                <svg viewBox="0 0 24 24" className="h-4 w-4" aria-hidden>
+                <svg viewBox="0 0 24 24" className="h-6 w-6 scale-110" aria-hidden>
                   <path fill="#25F4EE" d="M14.2 3.1c.5 2.6 2.2 4.4 4.8 4.8v3.1c-1.6 0-3.1-.5-4.4-1.3v6.6c0 3.4-2.7 6.1-6.1 6.1S2.4 19.7 2.4 16.3 5.1 10.2 8.5 10.2c.4 0 .8 0 1.1.1v3.2c-.3-.1-.7-.2-1.1-.2-1.6 0-2.9 1.3-2.9 3s1.3 3 2.9 3 2.9-1.3 2.9-3V3.1h2.8z" />
+                  <path fill="#FE2C55" d="M13.6 2.4c.5 2.6 2.2 4.4 4.8 4.8v3.1c-1.6 0-3.1-.5-4.4-1.3v6.6c0 3.4-2.7 6.1-6.1 6.1S1.8 19 1.8 15.6 4.5 9.5 7.9 9.5c.4 0 .8 0 1.1.1v3.2c-.3-.1-.7-.2-1.1-.2-1.6 0-2.9 1.3-2.9 3s1.3 3 2.9 3 2.9-1.3 2.9-3V2.4h2.8z" opacity=".85" />
                   <path fill="#fff" d="M13.8 2.6c.5 2.6 2.2 4.4 4.8 4.8v2.4c-1.6 0-3.1-.5-4.4-1.3v6.6c0 3.4-2.7 6.1-6.1 6.1S2 18.5 2 15.1 4.7 9 8.1 9c.4 0 .8 0 1.1.1v2.4c-.3-.1-.7-.2-1.1-.2-1.6 0-2.9 1.3-2.9 3s1.3 3 2.9 3 2.9-1.3 2.9-3V2.6h2.8z" />
                 </svg>
               </span>
             ) : (
               // eslint-disable-next-line @next/next/no-img-element
-              <img src={item.src} alt={item.label} className="h-full w-full object-cover" />
+              <img
+                src={item.src}
+                alt={item.label}
+                className={`h-full w-full object-cover ${item.id === "xiaohongshu" ? "scale-125" : ""}`}
+              />
             )}
           </a>
         ))}
