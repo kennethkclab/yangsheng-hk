ba2493e2c0a42a78a337ce481d683364711041c9
kennethkclab <291267340+kennethkclab@users.noreply.github.com>
2026-09-20 10:54:25 +0800

    allow more Next image quality values

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 2592000,
     deviceSizes: [360, 640, 750, 828, 1080, 1200, 1920],
     imageSizes: [32, 48, 64, 96, 128, 256, 384],
-    qualities: [60, 75, 80],
+    qualities: [60, 70, 75, 80],
     remotePatterns: [
       { protocol: "https", hostname: "yangsheng.hk" },
       { protocol: "https", hostname: "yangshengdao.hk" },
