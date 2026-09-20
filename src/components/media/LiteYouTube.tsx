0ad2e250db3cd195b60b4afb1acfd071ab64f04a
kennethkclab <291267340+kennethkclab@users.noreply.github.com>
2026-09-20 11:33:09 +0800

    shrink YouTube play button

--- src/components/media/LiteYouTube.tsx
+++ src/components/media/LiteYouTube.tsx
@@ -13,9 +13,8 @@ export default function LiteYouTube({
 }) {
   const [play, setPlay] = useState(false);
   const ratio = aspect === "short" ? "aspect-[9/16]" : "aspect-video";
-  const thumb = aspect === "short"
-    ? `https://i.ytimg.com/vi/${id}/hqdefault.jpg`
-    : `https://i.ytimg.com/vi/${id}/hqdefault.jpg`;
+  const thumb = `https://i.ytimg.com/vi/${id}/hqdefault.jpg`;
+  const size = 44;
 
   return (
     <div className={`relative ${ratio} overflow-hidden bg-stone-900`}>
@@ -32,10 +31,10 @@ export default function LiteYouTube({
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img src={thumb} alt={title} className="absolute inset-0 h-full w-full object-cover" />
           <span
-            className="pointer-events-none absolute left-1/2 top-1/2 block rounded-full bg-[#FF0000] shadow-lg"
-            style={{ width: 68, height: 68, marginLeft: -34, marginTop: -34 }}
+            className="pointer-events-none absolute left-1/2 top-1/2 block rounded-full bg-[#FF0000] shadow-md"
+            style={{ width: size, height: size, marginLeft: -size / 2, marginTop: -size / 2 }}
           >
-            <svg viewBox="0 0 24 24" width="28" height="28" className="absolute left-1/2 top-1/2 -translate-x-[46%] -translate-y-1/2" aria-hidden>
+            <svg viewBox="0 0 24 24" width="16" height="16" className="absolute left-1/2 top-1/2 -translate-x-[42%] -translate-y-1/2" aria-hidden>
               <path fill="#fff" d="M8 5.5v13l11-6.5L8 5.5z" />
             </svg>
           </span>
